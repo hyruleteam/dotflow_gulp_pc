8077c70ee80a51675aad3e0102450a18b3fadc71
王文宇 <wangwenyu9394@gmail.com>
2018-08-18 17:13:54 +0800

    update:gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,127 @@
+const fs = require('fs');
+const path = require('path');
+
 const gulp = require('gulp');
-const minifyCSS = require('gulp-minify-css');
+const fileinclude = require('gulp-file-include');
 const sass = require('gulp-sass');
-const spritesmith = require('gulp.spritesmith');
-const uglify = require('gulp-uglify');
-const autoprefixer = require('gulp-autoprefixer');
-const merge = require('merge-stream');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
+const comments = require('postcss-discard-comments');
 const clean = require('gulp-clean');
 const browserSync = require('browser-sync').create();
 const plumber = require('gulp-plumber');
 const changed = require('gulp-changed');
 const watch = require('gulp-watch');
 const sourcemaps = require('gulp-sourcemaps');
-const fileinclude =   require('gulp-file-include');
+
+const rollup = require('gulp-rollup');
+const babel = require('rollup-plugin-babel')
+const resolve = require('rollup-plugin-node-resolve');
+
+const rev = require('gulp-revm');
+const revCollector = require('gulp-revm-collector');
 
 //编译路径
 const devPath = './src';
+const tempPath = './temp';
 const distPath = './dist';
+
+const compileHtml = () => {
+    return gulp
+        .src([`${devPath}/html/**/*.html`,`!${devPath}/html/include/*.html`])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(plumber())
+}
+
+const compileStyle = () => {
+    const plugins = [
+        autoprefixer(['iOS >= 7', 'Android >= 4.1']),
+        comments()
+    ];
+
+    return gulp
+        .src([`${devPath}/sass/*.scss`])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(changed(`${tempPath}/css/`, {
+            hasChanged: changed.compareContents,
+            extension: '.css'
+        }))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(postcss(plugins))
+        .pipe(sourcemaps.write('.'))
+}
+
+
+//读取入口文件
+let readdirfile = []
+const readFile = (path) =>{
+    readdirfile = fs.readdirSync(path,{encoding:'utf-8'})
+}
+
+const readEntryFile = async () =>{
+    const filePath = path.join(__dirname, `${devPath}/js`)
+
+    readFile(filePath)
+
+    return readdirfile
+        .filter(el => /\.js/.test(el))
+        .map(item => `${devPath}/js/${item}`)
+}
+
+const compileJs = () => {
+    const fileList = readEntryFile();
+    return gulp
+        .src([`${devPath}/js/**/*.js`])
+        .pipe(plumber())
+        .pipe(changed(`${tempPath}/js/`, {hasChanged: changed.compareContents}))
+        .pipe(sourcemaps.init())
+        .pipe(rollup({
+            input: fileList,
+            output: {
+                format: 'es'
+            },
+            plugins: [
+                resolve(),
+                babel({
+                    exclude: 'node_modules/**',
+                    babelrc: false,
+                    presets: [['@babel/preset-env', {modules: false}]]
+                })
+            ]
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+}
+
+const compileImages = () => {
+    return gulp
+        .src([`${devPath}/images/**/*`, `!${devPath}/images/sprites/**/*`])
+        .pipe(plumber())
+}
+
+const compileFonts = () => {
+    return gulp
+        .src([`${devPath}/font/**/*`])
+        .pipe(plumber())
+}
+
+const compileLib = () => {
+    return gulp
+        .src([`${devPath}/lib/**/*`])
+        .pipe(plumber())
+}
 
 //browser-sync
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
-            baseDir: distPath
+            baseDir: tempPath
         },
         port: 8091,
         notify: false, //刷新是否提示
@@ -31,184 +131,135 @@ gulp.task('browser-sync', () => {
 });
 
 //html
-gulp.task('buildHtml', () => {
-    return watch(`${devPath}/html/**/*.html`, () => {
-        gulp
-            .src([`${devPath}/html/**/*.html`, `!${devPath}/html/include/*.html`])
-            .pipe(plumber())
-            .pipe(changed(`${distPath}/html`, {hasChanged: changed.compareContents}))
-            .pipe(fileinclude({prefix: '@@', basepath: '@file'}))
-            .pipe(gulp.dest(`${distPath}/html`))
+gulp.task('compileHtml', () => {
+    return watch(`${devPath}/html/**/*`, () => {
+        compileHtml()
+            .pipe(gulp.dest(`${tempPath}/html`))
             .pipe(browserSync.stream());
     })
 });
 
 //style
-gulp.task('buildStyle', () => {
+gulp.task('compileStyle', () => {
     return watch(`${devPath}/sass/**/*.scss`, () => {
-        gulp
-            .src(`${devPath}/sass/*.scss`)
-            .pipe(plumber())
-            .pipe(sourcemaps.init())
-            .pipe(changed(`${distPath}/css`, {
-                hasChanged: changed.compareContents,
-                extension: '.css'
-            }))
-            .pipe(sass().on('error', sass.logError))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest(`${distPath}/css`))
+        compileStyle()
+            .pipe(gulp.dest(`${tempPath}/css/`))
             .pipe(browserSync.stream()); //browserSync:只监听sass编译之后的css
     })
 });
 
 //js
-gulp.task('buildJs', () => {
-    const pageJs = watch(`${devPath}/js/pages/**/*.js`, () => {
-        gulp
-            .src([`${devPath}/js/pages/**/*.js`])
-            .pipe(plumber())
-            .pipe(changed(`${distPath}/js/pages/`, {hasChanged: changed.compareContents}))
-            .pipe(gulp.dest(`${distPath}/js/pages/`))
+gulp.task('compileJs', () => {
+    return watch(`${devPath}/js/**/*.js`, () => {
+        compileJs()
+            .pipe(gulp.dest(`${tempPath}/js/`))
             .pipe(browserSync.stream());
     })
-
-    const vendorJs = watch(`${devPath}/js/vendor/**/*`, () => {
-        gulp
-            .src([`${devPath}/js/vendor/**/*`])
-            .pipe(plumber())
-            .pipe(gulp.dest(`${distPath}/js/vendor/`))
-            .pipe(browserSync.stream());
-    })
-
-    return merge(pageJs, vendorJs);
 });
 
 //images
-gulp.task('buildImage', () => {
-    return watch(`${devPath}/img/**/*`, () => {
-        gulp
-            .src([`${devPath}/img/**/*`])
-            .pipe(plumber())
-            .pipe(gulp.dest(`${distPath}/img`))
+gulp.task('compileImages', () => {
+    return watch(`${devPath}/images/**/*`, () => {
+        compileImages()
+            .pipe(gulp.dest(`${tempPath}/images/`))
             .pipe(browserSync.stream())
     })
 });
 
 //font
-gulp.task('buildFont', () => {
+gulp.task('compileFonts', () => {
     return watch(`${devPath}/font/**/*`, () => {
-        gulp
-            .src([`${devPath}/font/**/*`])
-            .pipe(plumber())
-            .pipe(gulp.dest(`${distPath}/font/`))
+        compileFonts()
+            .pipe(gulp.dest(`${tempPath}/font/`))
             .pipe(browserSync.stream())
     })
 });
 
-//sprites
-const argv = require('minimist')(process.argv.slice(2));
-
-const spritesMithConfig = {
-    imgName: argv.name + '.png',
-    cssName: '_' + argv.name + '.scss',
-    cssFormat: 'scss',
-    algorithm: 'binary-tree',
-    imgPath: '../images/sprite/' + argv.name + '.png',
-    padding: 10
-}
-
-gulp.task('buildSprite', () => {
-    const spriteData = gulp
-        .src(`${devPath}/images/sprites/${argv.name}/*`)
-        .pipe(plumber())
-        .pipe(spritesmith(spritesMithConfig));
-
-    spriteData
-        .img
-        .pipe(gulp.dest(`${devPath}/images/sprite`));
-    spriteData
-        .img
-        .pipe(gulp.dest(`${distPath}/images/sprite`));
-    spriteData
-        .css
-        .pipe(gulp.dest(`${devPath}/sass/module/icon`));
+//lib
+gulp.task('compileLib', () => {
+    return watch(`${devPath}/lib/**/*`, () => {
+        compileLib()
+            .pipe(gulp.dest(`${tempPath}/lib/`))
+            .pipe(browserSync.stream());
+    })
 });
 
 //clean
-gulp.task('buildClean', () => {
+gulp.task('compileClean', () => {
     return gulp
-        .src([`${distPath}`])
+        .src([`${tempPath}`])
         .pipe(plumber())
         .pipe(clean({force: true}))
 });
 
 //devPack
-gulp.task('devPack', ['buildStyle', 'buildHtml', 'buildJs', 'buildImage', 'buildFont']);
+gulp.task('compile', ['compileHtml', 'compileStyle', 'compileJs', 'compileImages', 'compileFonts']);
 
-//buildPack
-gulp.task('buildAssets', () => {
-    const html = gulp
-        .src([`${devPath}/html/**/*.html`, `!${devPath}/html/include/*.html`])
-        .pipe(plumber())
-        .pipe(changed(`${distPath}/html`, {hasChanged: changed.compareContents}))
-        .pipe(fileinclude({prefix: '@@', basepath: '@file'}))
-        .pipe(gulp.dest(`${distPath}/html`))
-        .pipe(browserSync.stream());
-
-    const styles = gulp
-        .src([`${devPath}/sass/*.scss`])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'last 2 Explorer versions', '> 5%']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(`${distPath}/css`))
-
-    const pagejs = gulp
-        .src(`${devPath}/js/**/*.js`)
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest(`${distPath}/js`));
-
-    const vendorJs = gulp
-        .src([`${devPath}/js/vendor/**/*`])
-        .pipe(plumber())
-        .pipe(gulp.dest(`${distPath}/js/vendor/`))
-
-    const fonts = gulp
-        .src([`${devPath}/font/**/*`])
-        .pipe(plumber())
-        .pipe(gulp.dest(`${distPath}/font/`))
-
-    const images = gulp
-        .src([`${devPath}/images/**/*`, `!${devPath}/images/sprites/**/*`])
-        .pipe(gulp.dest(`${distPath}/images`))
-
-    return merge(styles, pagejs, vendorJs, fonts, images, html);
+//compileAssets
+gulp.task('compileAssets', ['compileClean'], () => {
+    compileHtml().pipe(gulp.dest(`${tempPath}/html`));
+    compileStyle().pipe(gulp.dest(`${tempPath}/css`));
+    compileJs().pipe(gulp.dest(`${tempPath}/js`));
+    compileImages().pipe(gulp.dest(`${tempPath}/images`));
+    compileFonts().pipe(gulp.dest(`${tempPath}/fonts`));
+    compileLib().pipe(gulp.dest(`${tempPath}/lib`));
 });
 
-gulp.task('buildRevPack', ['buildAssets'], () => {
-    const styles = gulp
-        .src([`${distPath}/css/*.css`])
+gulp.task('default', [
+    'compileAssets', 'compile','browser-sync'
+], () => {
+    gulp
+        .watch([`${tempPath}/**/*.+(html|css|js|png|jpg|ttf)`])
+        .on('change', browserSync.reload);
+});
+
+
+/**
+ * 打包文件
+ */
+
+gulp.task('buildVersion',()=>{
+    return gulp
+        .src([`${distPath}/rev/**/*.json`,`${distPath}/html/**/*.html`])
         .pipe(plumber())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'last 2 Explorer versions', '> 5%']
+        .pipe(revCollector({
+            replaceReved: true
         }))
-        .pipe(minifyCSS())
+        .pipe(gulp.dest(`${distPath}/html`))
+})
+
+gulp.task('buildAssets',()=>{
+    compileHtml()
+        .pipe(gulp.dest(`${distPath}/html`))
+
+    compileStyle()
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write())
         .pipe(rev())
-        .pipe(gulp.dest(`${distPath}/css/`))
+        .pipe(gulp.dest(`${distPath}/css`))
         .pipe(rev.manifest())
         .pipe(gulp.dest(`${distPath}/rev/css`))
 
-    return merge(styles);
+    compileJs()
+        .pipe(rev())
+        .pipe(gulp.dest(`${distPath}/js`))
+        .pipe(rev.manifest())
+        .pipe(gulp.dest(`${distPath}/rev/js`))
+
+    compileImages().pipe(gulp.dest(`${distPath}/images`))
+    compileFonts().pipe(gulp.dest(`${distPath}/fonts`))
+    compileLib().pipe(gulp.dest(`${distPath}/lib`))
 })
 
-gulp.task('default', [
-    'buildAssets', 'devPack', 'browser-sync'
-], () => {
-    gulp
-        .watch([`${distPath}/**/*.+(css|js|png|jpg|ttf|html)`])
-        .on('change', browserSync.reload);
-});
+
+//清理文件
+gulp.task('buildClean',()=>{
+    return gulp
+        .src([`${distPath}`])
+        .pipe(plumber())
+        .pipe(clean({force: true}))
+})
+
+//打包文件
+gulp.task('build',['buildAssets'],()=>{})
